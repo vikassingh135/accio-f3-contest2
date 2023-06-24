7b0276ed524692ebd861391b8b7884c0af78a09b
vikassingh135 <vikassingh1352003@gmail.com>
2023-06-24 23:08:32 +0530

    done

---
+++ index.js
@@ -0,0 +1,69 @@
+window.onload = () => {
+  if (isAccessToken()) {
+    alert("Already Signed Up");
+    window.location.href = "/profile.html";
+  }
+};
+
+const signUp = () => {
+  event.preventDefault();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("confirm-password").value;
+
+  const messageDiv = document.getElementById("message-div");
+  messageDiv.innerHTML = "";
+
+  if (!document.getElementById("email").validity.valid) {
+    messageDiv.innerText = "Error : Please provide correct email address";
+    messageDiv.style.color = "#FF4F4F";
+    return;
+  }
+
+  if (!(name && email && password && confirmPassword)) {
+    messageDiv.innerText = "Error : All the fields are mandatory";
+    messageDiv.style.color = "#FF4F4F";
+    return;
+  }
+
+  if (password !== confirmPassword) {
+    messageDiv.innerText =
+      "Error : Password and confirm password does not match";
+    messageDiv.style.color = "#FF4F4F";
+    return;
+  }
+
+  messageDiv.innerText = "Successfully Signed Up!";
+  messageDiv.style.color = "#7ECD71";
+
+  let accessToken = generateAccessToken();
+
+  console.log(accessToken);
+  const userState = { name, email, password, accessToken };
+
+  localStorage.setItem("userState", JSON.stringify(userState));
+  setTimeout(() => (window.location.href = "/profile.html"), 500);
+};
+
+// for generating access token
+const generateAccessToken = () => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+
+  for (let i = 0; i < 16; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+
+  return result;
+};
+
+const isAccessToken = () => {
+  if (!localStorage.getItem("userState")) return false;
+  const user = localStorage.getItem("userState");
+  const userObj = JSON.parse(user);
+  if (!userObj.accessToken) return false;
+  return true;
+};
---
+++ profile.js
@@ -0,0 +1,31 @@
+window.onload = () => {
+  if (!isAccessToken()) {
+    alert("Please Sign Up First");
+    window.location.href = "/index.html";
+  }
+  displayUser();
+};
+
+const displayUser = () => {
+  const user = localStorage.getItem("userState");
+  const userObj = JSON.parse(user);
+  document.getElementById("name").innerText += userObj.name;
+  document.getElementById("email").innerText += userObj.email;
+  document.getElementById("password").innerText += userObj.password;
+};
+
+const logOut = () => {
+  if (localStorage.getItem("userState")) localStorage.removeItem("userState");
+  window.location.href = "/index.html";
+};
+
+const isAccessToken = () => {
+  if (!localStorage.getItem("userState")) return false;
+  const user = localStorage.getItem("userState");
+  const userObj = JSON.parse(user);
+  // console.log(user,userObj, userObj.accessToken);
+  if (!userObj.accessToken) return false;
+  return true;
+};
+
+
